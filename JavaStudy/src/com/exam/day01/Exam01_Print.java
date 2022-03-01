06f3d73c88bd8529c03f8c0fb3ddcaaad45fb551
bontaedev <kbt6882965@naver.com>
2022-03-02 01:42:39 +0900

    Create example 01

---
+++ JavaStudy/src/com/exam/day01/Exam01_Print.java
@@ -0,0 +1,15 @@
+package com.exam.day01;
+
+public class Exam01_Print {
+	public static void main(String[] args) {
+		// 다룬 줄에 A,B,C를 출력
+		System.out.println("A");
+		System.out.println("B");
+		System.out.println("C");
+		
+		// 같은 줄에 A,B,C를 출력
+		System.out.print("A");
+		System.out.print("B");
+		System.out.print("C");
+	}
+}
