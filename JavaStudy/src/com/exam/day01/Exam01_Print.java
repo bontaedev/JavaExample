e2e25ce32b4fe6b9754bba8acac696a0b6d52762
bontaedev <kbt6882965@naver.com>
2022-03-09 19:04:04 +0900

    upload day01 example files

--- JavaStudy/src/com/exam/day01/Exam01_Print.java
+++ JavaStudy/src/com/exam/day01/Exam01_Print.java
@@ -11,5 +11,17 @@ public class Exam01_Print {
 		System.out.print("A");
 		System.out.print("B");
 		System.out.print("C");
+		
+		// Double quotation vs Single quotation
+		// single quotation은 문자에서만, 문자열은 반드시 "" 안에!
+		System.out.println("a");
+		System.out.println('가');
+		// System.out.println('가나다'); // error
+		
+		// 숫자도 출력가능
+		System.out.println(50);
+		System.out.println(0.48);
+		System.out.println(-5.55);
+		
 	}
 }
